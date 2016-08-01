b3052f8ce0af5379eb196d4b904bf2ec551d6b95
Ted Fujimoto <tfturing@gmail.com>
2016-07-31 22:51:17 -0700

    added google-maps-AtoB.py class

---
+++ server/app/google-maps-AtoB.py
@@ -0,0 +1,25 @@
+"""
+A simple GoogleMaps API wrapper.
+
+Necessary steps:
+1. Get a Google Maps API key here: https://developers.google.com/maps/documentation/static-maps/
+2. Include the API key in the constructor or assign it as an environment variable.
+3. Let A be the starting point (an address or (lat, long)) and B be the destination.
+Then create a GoogleMapsAtoB object (e.g. g = GoogleMapsAtoB(A, B))
+4. Get distance from A to B using the 'get_distance' method and the time using the
+'get_time' method.
+"""
+
+import googlemaps as gm
+from datetime import datetime
+
+class GoogleMapsAtoB:
+    def __init__(self, src, dst, api_key=API_KEY, date=datetime.now):
+        self.client = gm.Client(api_key)
+        self.directions = self.client.directions(src, dst, date)
+
+    def get_distance(self):
+        return float(self.directions[0]["legs"][0]["distance"]["text"].split(" ")[0])
+
+    def get_time(self):
+        return float(self.directions[0]["legs"][0]["duration"]["text"].split(" ")[0])
