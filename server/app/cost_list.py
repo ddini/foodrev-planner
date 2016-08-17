051030c1f6fbe7e1e916354ed75c64e7e95ab6fe
Ted Fujimoto <tfturing@gmail.com>
2016-08-16 21:02:22 -0700

    Added comments to cost_list.py

--- server/app/cost_list.py
+++ server/app/cost_list.py
@@ -1,11 +1,11 @@
 import csv
-from itertools import combinations
+import itertools
 from googlemapsAtoB import GoogleMapsAtoB
 
 """
-input: a list of addresses and the name of the csv file.
-output: a csv file of all the combinations of pairs of addresses and their
-    associated cost (in this case, time).
+Constructor Inputs:
+  addresses - a list of addresses (or coordinates)
+  api_key - a Google Maps API key
 """
 class CostList:
     def __init__(self, addresses, api_key):
@@ -13,14 +13,27 @@ class CostList:
         self.api_key = api_key
 
     def cost_list(self):
-        address_pairs = combinations(self.addresses, 2)
+        # Ideally, we want the itertools.permutation function instead since
+        # itertools.combinations assumes the time between point A and point B is
+        # symmetric (that is, the time from A to B is the same as B to A).
+        # Using the itertools.permutation function, however, approximately doubles
+        # the amount of time it takes to retrieve the needed information from the
+        # Google Maps API (makes sense because we are doubling the pairs of addresses).
+        # Hence, we will make this simplifying assumption for now until we find
+        # a solution that makes using permutations more efficient.
+        address_pairs = itertools.combinations(self.addresses, 2)
         return [(a, b, GoogleMapsAtoB(a, b, self.api_key).get_time()) for a, b in address_pairs]
 
-    def to_csv(self, csv_name):
+    """
+    input: the name of the csv file.
+    output: a csv file of all the combinations of pairs of addresses and their
+        associated cost (in this case, time).
+    """
+    def to_csv(self, csv_name, delimiter='*'):
         cost_list_to_csv = self.cost_list()
 
         with open(csv_name, 'wb') as csvfile:
-            output = csv.writer(csvfile)
+            output = csv.writer(csvfile, delimiter=delimiter)
             output.writerow(["address1", "address2", "cost"])
             for a, b, cost in cost_list_to_csv:
                 output.writerow([a, b, cost])
