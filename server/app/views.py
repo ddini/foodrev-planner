334e24baeb42cb2362b063d95039d211492a4657
Don M Dini <ddini@datacore.gateway.pace.com>
2016-09-11 17:19:26 -0700

    Create session functioning.

--- server/app/views.py
+++ server/app/views.py
@@ -3,6 +3,8 @@ from app import app
 from flask import request
 
 import plan_interface
+import hashlib
+import datetime
 
 @app.route('/')
 @app.route('/index')
@@ -15,6 +17,33 @@ def echo_payload():
     print "JSON Payload: %s" % json_data
     
     return str(json_data)
+
+def get_session_hash(current_time, requester_ip):
+    md5 = hashlib.md5()
+    md5.update(str(current_time))
+    md5.update(str(requester_ip))
+
+    return md5.hexdigest()
+
+
+@app.route("/createsession", methods=["PUT"])
+def create_session():
+    
+    #Generate session hash
+    #---------------------
+    current_time = datetime.datetime.now()
+    requester_ip = request.remote_addr
+    
+    session_hash = get_session_hash(current_time, requester_ip)
+    #---------------------
+
+    json_data = {"session_id":session_hash}
+
+    return str(json_data)
+
+@app.route("/chooseplan", methods=["PUT"])
+def choose_plan():
+    pass
 
 @app.route("/plan", methods=["POST", "PUT"])
 def get_plan():
